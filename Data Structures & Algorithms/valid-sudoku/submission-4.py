a6940cdbed8099502cd7f7cec2e7d4754b7ba130
Ajesh Vyas <65610953+ajeshvyas@users.noreply.github.com>
2026-08-19 06:01:40 +0530

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,62 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # for row in board:
+        #     row_ele = []
+        #     for ele in row:
+        #         if ele.isnumeric():
+        #             if ele in row_ele:
+        #                 return False
+        #             row_ele.append(ele)
+        # for col in range(len(board)):
+        #     col_ele = []
+        #     for ele in range(len(board)):
+        #         if board[ele][col].isnumeric():
+        #             if board[ele][col] not in col_ele:
+        #                 col_ele.append(board[ele][col])
+        #             else:
+        #                 return False
+        # col_vector = 0
+        # row_vector = 0
+        # while row_vector < 9:
+        #     box_ele = []
+        #     for row in range(row_vector, row_vector + 3):
+        #         for col in range(col_vector, col_vector + 3):
+        #             value = board[row][col]
+        #             if value.isnumeric():
+        #                 if value in box_ele:
+        #                     return False
+        #                 box_ele.append(value)
+        #     col_vector += 3
+        #     if col_vector == 9:
+        #         col_vector = 0
+        #         row_vector += 3
+        # return True
+
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for row in range(9):
+            for col in range(9):
+
+                value = board[row][col]
+
+                if value == ".":
+                    continue
+
+                box = (row // 3) * 3 + (col // 3)
+
+                if value in rows[row]:
+                    return False
+
+                if value in cols[col]:
+                    return False
+
+                if value in boxes[box]:
+                    return False
+
+                rows[row].add(value)
+                cols[col].add(value)
+                boxes[box].add(value)
+
+        return True
