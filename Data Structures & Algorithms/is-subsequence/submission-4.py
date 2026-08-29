51588c7572f9cf889400e9756731585579e8497c
Ajesh Vyas <65610953+ajeshvyas@users.noreply.github.com>
2026-08-29 06:39:36 +0530

    Add: is-subsequence - submission-4

---
+++ Data Structures & Algorithms/is-subsequence/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # t_map = dict()
+        # for char in t:
+        #     t_map[char] = t_map.get(char, 0)+1
+        
+        # for char in s:
+        #     if t_map.get(char, None) and t_map[char] > 0:
+        #         t_map[char] -= 1
+        #     else:
+        #         return False
+        # return True
+
+        i = 0
+
+        for char in t:
+            if i < len(s) and s[i] == char:
+                i += 1
+
+        return i == len(s)
